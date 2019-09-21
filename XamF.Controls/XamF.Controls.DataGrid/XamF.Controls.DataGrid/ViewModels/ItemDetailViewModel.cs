1fe98c6d693de5e3b4ecc016ea93f32fe9f03cc5
Abdulrahman Emad <aemad@integrant.com>
2019-09-21 14:20:50 +0200

    create xamarin projects

---
+++ XamF.Controls/XamF.Controls.DataGrid/XamF.Controls.DataGrid/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using XamF.Controls.DataGrid.Models;
+
+namespace XamF.Controls.DataGrid.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
